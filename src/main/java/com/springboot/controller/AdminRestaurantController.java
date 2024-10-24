8d77b1f2703eb216b6aba05d91cfe56aa36edfc5
Anand Kumar <111699706+Anand98013@users.noreply.github.com>
2024-10-24 20:41:20 +0530

    Update AdminRestaurantController.java

--- src/main/java/com/springboot/controller/AdminRestaurantController.java
+++ src/main/java/com/springboot/controller/AdminRestaurantController.java
@@ -21,7 +21,7 @@ import com.springboot.response.MessageResponse;
 import com.springboot.services.RestaurantService;
 import com.springboot.services.UserServices;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://mealminglefood.vercel.app")
 @RestController
 @RequestMapping("/api/admin/restaurants")
 public class AdminRestaurantController {
